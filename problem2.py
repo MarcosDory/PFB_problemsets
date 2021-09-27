840ef4963c80ffa35b464e115f35ec520d2b3769
MarcosDory <marcosalexandre1940@hotmail.com>
2021-09-27 23:49:33 +0000

    add file 2

---
+++ problem2.py
@@ -0,0 +1,23 @@
+DNA="GTACCTTGATTTCGTATTCTGAGAGGCTGCTGCTTAGCGGTAGCCCCTTGGTTTCCGTGGCAACGGAAAA"
+if 'ATG' in DNA:
+ print('ATG found in DNA')
+else:
+ print('ATG is not found in DNA')
+if 'TTT' in DNA:
+ print('TTT found in DNA')
+year=2021
+if year < 2021:
+ print(true)
+else:
+ print('Not True')
+note=50
+if note > 0:
+ print('positive')
+elif note < 0:
+ print('negative')
+elif note > 50:
+ print(note,'smaller than 50')
+elif note < 50:
+ print(note,'higher than 50')
+else: 
+ print('found 50')
